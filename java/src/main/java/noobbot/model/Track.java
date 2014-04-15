d802c10e6b771b18ff9f626784a001df8df10b5d
Makkis <marko.j.berg@gmail.com>
2014-04-15 18:19:49 +0300

    Track class and Position interface.

---
+++ java/src/main/java/noobbot/model/Track.java
@@ -0,0 +1,17 @@
+package noobbot.model;
+
+import noobbot.descriptor.GameInitDescriptor;
+
+public class Track {
+
+	
+	
+	public Track(GameInitDescriptor.Data.Race.Track trackDescriptor) {
+		
+	}
+	
+	public Double getDistance(Position position1, Position position2) {
+
+		return 0.0;
+	}
+}
