dc87c8f8dd06cfd7d510892cd67ede329f29886b
Sauli Tähkäpää <sauli@vaadin.com>
2014-04-15 17:10:31 +0300

    More track classes.

--- java/src/main/java/noobbot/GameInit.java
+++ java/src/main/java/noobbot/GameInit.java
@@ -1,16 +1,21 @@
 package noobbot;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Saulis on 15/04/14.
  */
 public class GameInit {
     public class Data {
+        public class Race {
         public class Track {
             public class Piece {
                 public double length;
                 public double radius;
                 public double angle;
-                //public boolean switch;
+
+                @SerializedName("switch")
+                public boolean hasSwitch;
             }
 
             public class Lane {
@@ -20,17 +25,42 @@ public class GameInit {
 
             public class StartingPoint {
                 public class Position {
-                    
+                    public double x;
+                    public double y;
                 }
+
+                public Position position;
+                public double angle;
             }
 
             public String id;
             public String name;
             public Piece[] pieces;
             public Lane[] lanes;
+            public StartingPoint startingPoint;
+        }
+
+        public class Car {
+            public class Id {
+                public String name;
+                public String color;
+            }
+
+            public class Dimensions {
+                public double length;
+                public double width;
+                public double guideFlagPosition;
+            }
+
+            public Id id;
+            public Dimensions dimensions;
         }
 
         public Track track;
+        public Car[] cars;
+    }
+        
+        public Race race;
     }
 
     public String msgType;
